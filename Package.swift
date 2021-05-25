92bb3179890cbbf2a58854077839518220eba8c1
Dmytro Medynskyi <dmytro.medynskyi@skelia.com>
2021-05-25 14:20:54 +0300

    rename

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "ImageMagick7",
+    name: "ImageMagick7Remote",
     platforms: [
         .iOS(.v14)
     ],
-    products: [.library(name: "ImageMagick7", targets: ["ImageMagick7"])],
+    products: [.library(name: "ImageMagick7Remote", targets: ["ImageMagick7Remote"])],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "ImageMagick7", path: "Sources/ImageMagick7.xcframework"),
+        .binaryTarget(name: "ImageMagick7Remote", path: "Sources/ImageMagick7.xcframework"),
     ]
 )
